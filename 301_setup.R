2f8a045c45494e8608a0fbf62c49f2381668ed92
Harmony Luce <153374146+doctorhluce@users.noreply.github.com>
2024-12-03 18:00:37 -0600

    Create 301_setup.R

---
+++ 301_setup.R
@@ -0,0 +1,13 @@
+
+# clear environment
+rm(list = ls())
+
+# load libraries
+library(tidyverse)
+library(mosaic)
+library(scales)
+library(moderndive)
+library(regclass)
+
+# import data
+data(TIPS)
